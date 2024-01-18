1c72f0c94274f3664ff7ede714915f15ad3e2a8c
Oleksandr B. <oleksandr.bi@paymentop.com>
2024-01-18 10:48:55 +0200

    Update which-key message

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,6 +1,8 @@
 return {
-  -- "folke/which-key.nvim",
-  -- opts = {
-  --   ["gs"] = ,
-  -- },
+  "folke/which-key.nvim",
+  opts = function(_, opts)
+    if require("lazyvim.util").has("noice.nvim") then
+      opts.defaults["<leader>fn"] = { name = "+noice" }
+    end
+  end,
 }
