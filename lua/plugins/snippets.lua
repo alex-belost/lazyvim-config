f6570296f3c63927ab83e986c7afe0e2dc2d73bf
Oleksandr B. <oleksandr.bi@paymentop.com>
2024-01-18 13:12:17 +0200

    Add global shippets handler

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,8 +1,5 @@
 return {
   "L3MON4D3/LuaSnip",
-  keys = function()
-    return {}
-  end,
   config = function(_, opts)
     if opts then
       require("luasnip").config.setup(opts)
@@ -12,7 +9,8 @@ return {
       require("luasnip.loaders.from_" .. type).lazy_load()
     end, { "vscode", "snipmate", "lua" })
 
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./lua/snippets/code-spinnets" } })
-    -- require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets/lua-snippets" } })
+    vim.tbl_map(function(type)
+      require("luasnip.loaders.from_" .. type).lazy_load({ paths = { "./lua/snippets/" .. type } })
+    end, { "vscode" })
   end,
 }
