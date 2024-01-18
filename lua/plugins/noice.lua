1a0cefa4c85d74361b7643e712e3a64f1dc53f13
Oleksandr B. <oleksandr.bi@paymentop.com>
2024-01-18 10:40:48 +0200

    Update noice.nvim keymaps

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,38 @@
+return {
+  "folke/noice.nvim",
+  keys = {
+    { "<leader>snl", false },
+    { "<leader>snh", false },
+    { "<leader>sna", false },
+    { "<leader>snd", false },
+
+    {
+      "<leader>fnl",
+      function()
+        require("noice").cmd("last")
+      end,
+      desc = "Noice Last Message",
+    },
+    {
+      "<leader>fnh",
+      function()
+        require("noice").cmd("history")
+      end,
+      desc = "Noice History",
+    },
+    {
+      "<leader>fna",
+      function()
+        require("noice").cmd("all")
+      end,
+      desc = "Noice All",
+    },
+    {
+      "<leader>fnd",
+      function()
+        require("noice").cmd("dismiss")
+      end,
+      desc = "Dismiss All",
+    },
+  },
+}
