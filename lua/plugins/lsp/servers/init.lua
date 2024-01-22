85f13b7ec47d14adb4eb4ae64aab652b0f3ed5d9
Oleksandr B. <oleksandr.bi@paymentop.com>
2024-01-22 13:03:56 +0200

    Update php intelephense

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -3,4 +3,5 @@ return {
   stylelint_lsp = require("plugins.lsp.servers.stylelint_lsp"),
   eslint = require("plugins.lsp.servers.eslint"),
   emmet_language_server = require("plugins.lsp.servers.emmet_language_server"),
+  intelephense = require("plugins.lsp.servers.intelephense"),
 }
